699a47a998523e728b8c319f25aad6107afd29cc
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-02 00:45:21 +0900

    Update indent.cpp

--- src/original/indent.cpp
+++ src/original/indent.cpp
@@ -1,11 +1,11 @@
 #include "../../hdr/prototype.h"
 
 void indent_(string& src){
-   src = "   " + src;
+   src = tab + src;
    int i = 0;
    while (src[i]){
-      if (src[i]=='\n') src.insert(i+1, "   ");
+      if (src[i]=='\n') src.insert(i+1, tab);
       ++i;
    }
-   src = src.substr(0, src.size()-3);
+   src = src.substr(0, src.size()-tab.size());
 }
