931d5dd96bc2c4e32271f47c44671c076ff23f2d
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-01 23:49:20 +0900

    Update in.cpp

--- src/original/in.cpp
+++ src/original/in.cpp
@@ -8,8 +8,9 @@ void in_(string& src){
          vector<P> v;
          int i = idx+3;
          bool flg = true;
+         string type, name;  string key;
          while (flg){
-            string type, name;  string key;
+            type = name = key = "";
             while (1){
                if (src[i]==')'){
                   flg = false;
@@ -35,7 +36,7 @@ void in_(string& src){
                         ++i;
                         break;
                      }
-                     else if (src[i]==' '){
+                     else if (src[i]==')'){
                         flg = false;
                         break;
                      }
@@ -71,7 +72,10 @@ void in_(string& src){
             }
             if (name==""){
                name = type;
-               if (!v.empty()) type = v[v.size()-1].first;
+               if (!v.empty()){
+                  if ((v[v.size()-1].first).find("[")) type = (v[v.size()-1].first).substr(0, (v[v.size()-1].first).find("["));
+                  else type = v[v.size()-1].first;
+               }
                else type = "int";
             }
             type += key;
@@ -80,16 +84,14 @@ void in_(string& src){
          }
          string strend = src.substr(i);
          src = src.substr(0, idx);
-         string type = v[0].first;
+         string typ = v[0].first;
          int i_s = 0, i_e = 0;
          i = 0;
          while (i <= v.size()){
-            if (i==v.size()) goto LABEL;
-            if (v[i].first==type) i_e = i;
+            if (v[i].first==typ && i!=v.size()) i_e = i;
             else {
-               LABEL:
-               if (type.find("[")==string::npos){
-                  src += type;
+               if (typ.find("[")==string::npos){
+                  src += typ;
                   for (int i = i_s; i <= i_e; ++i){
                      if (i!=i_s) src += ",";
                      src += " " + v[i].second;
@@ -104,16 +106,16 @@ void in_(string& src){
                }
                else {
                   int cnt = 0;
-                  for (int j = 0; j < type.size(); ++j) if (type[j]=='[') ++cnt;
+                  for (int j = 0; j < typ.size(); ++j) if (typ[j]=='[') ++cnt;
                   switch (cnt){
                      case 1: {
-                        src += type.substr(0, type.find("["));
-                        string k = type.substr(type.find("[")+1, type.find("]")-type.find("[")-1);
+                        src += typ.substr(0, typ.find("["));
+                        string k = typ.substr(typ.find("[")+1, typ.find("]")-typ.find("[")-1);
                         for (int i = i_s; i <= i_e; ++i){
                            if (i!=i_s) src += ",";
                            src += " " + v[i].second + "[" + k + "]";
+                           dict[v[i].second] = typ.substr(typ.find("[")+1, typ.find("]")-typ.find("[")-1);
                         }
-                        dict[v[i].second] = type.substr(type.find("[")+1, type.find("]")-type.find("[")-1);
                         src += ";\n";
                         src += "for (int i = 1; i <= " + k + "; ++i) cin";
                         for (int i = i_s; i <= i_e; ++i){
@@ -124,28 +126,28 @@ void in_(string& src){
                         break;
                      }
                      case 2: {
-                        src += type.substr(0, type.find("["));
-                        string n = type.substr(type.find("[")+1, type.find("]")-type.find("[")-1);
-                        string m = type.substr(type.rfind("[")+1, type.rfind("]")-type.rfind("[")-1);
+                        src += typ.substr(0, typ.find("["));
+                        string n = typ.substr(typ.find("[")+1, typ.find("]")-typ.find("[")-1);
+                        string m = typ.substr(typ.rfind("[")+1, typ.rfind("]")-typ.rfind("[")-1);
                         for (int i = i_s; i <= i_e; ++i){
                            if (i!=i_s) src += ",";
                            src += " " + v[i].second + "[" + n + "]" + "[" + m + "]";
+                           dict[v[i].second] = typ.substr(typ.find("[")+1, typ.find("]")-typ.find("[")-1) + ", " + typ.substr(typ.rfind("[")+1, typ.rfind("]")-typ.rfind("[")-1);
                         }
-                        dict[v[i].second] = type.substr(type.find("[")+1, type.find("]")-type.find("[")-1) + ", " + type.substr(type.rfind("[")+1, type.rfind("]")-type.rfind("[")-1);
                         src += ";\n";
                         src += "for (int i = 1; i <= " + n + "; ++i){\n";
-                        src += "   for (int j = 1; j <= " + m + "; ++j){\n      cin";
+                        src += tab + "for (int j = 1; j <= " + m + "; ++j){\n" + tab + tab + "cin";
                         for (int i = i_s; i <= i_e; ++i){
                            src += " >> " + v[i].second + "[i][j]";
                         }
-                        src += ";\n   }\n}\n";
+                        src += ";\n" + tab + "}\n}\n";
                         i_s =  i;  i_e = i;
                         break;
                      }
                   }
                }
             }
-            if (i!=v.size()) type = v[i].first;
+            if (i!=v.size()) typ = v[i].first;
             ++i;
          }
          src += strend;
