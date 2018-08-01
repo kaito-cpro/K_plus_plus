1b94e399a743c06361a9653a0ab9a1858bc3ef1e
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-01 18:32:40 +0900

    Update common.cpp

--- src/common.cpp
+++ src/common.cpp
@@ -1,1 +1,13 @@
-#include "common.h"
+#include "../hdr/common.h"
+
+int find(string src, string s){
+   int res = -1;
+   if (src.find(' '+s)!=string::npos) res = 1 + src.find(' '+s);
+   if (src.find('\n'+s)!=string::npos) res = 1 + src.find('\n'+s);
+   if (src.find(s)==0) res = 0;
+   return res;
+}
+
+string size(string arrayname){
+   return dict[arrayname];
+}
