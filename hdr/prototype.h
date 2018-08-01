b541e5bafefe135b00095953738554cd0b4ea3b8
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-01 18:32:06 +0900

    Create prototype.h

---
+++ hdr/prototype.h
@@ -0,0 +1,11 @@
+#ifndef _INC_PROTOTYPE_
+#define _INC_PROTOTYPE_
+#include "common.h"
+
+void include_(string& src);
+void indent_(string& src);
+void in_(string& src);
+void out_(string& src);
+void rep_(string& src);
+
+#endif
