dfa6d86bc27370f498092e584cb29dcfd828a593
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-04 21:03:35 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,52 @@ string tab = "    ";
 map<string, string> dict;
 
 int main(){
-   FILE* fpr;  //read
-   FILE* fpw;  //write
-   fpr = fopen("code/source_code.cpp", "r");
-   fpw = fopen("code/converted_code.cpp", "w");
+    char pro;
+    cout << "Problem: ";
+    cin >> pro;
 
-   string src;
-   char c[MAX];
-   while(fgets(c, MAX, fpr) != NULL) src += c;
+    FILE* fpr;  //read
+    FILE* fpw;  //write
+    switch (pro){
+        case 'a':
+            fpr = fopen("code/source_a.cpp", "r");
+            fpw = fopen("code/converted_a.cpp", "w");
+            break;
+        case 'b':
+            fpr = fopen("code/source_b.cpp", "r");
+            fpw = fopen("code/converted_b.cpp", "w");
+            break;
+        case 'c':
+            fpr = fopen("code/source_c.cpp", "r");
+            fpw = fopen("code/converted_c.cpp", "w");
+            break;
+        case 'd':
+            fpr = fopen("code/source_d.cpp", "r");
+            fpw = fopen("code/converted_d.cpp", "w");
+            break;
+    }
 
-   convert(src);
-   fprintf(fpw, "%s", src.c_str());
+    string src;
+    char c[MAX];
+    while(fgets(c, MAX, fpr) != NULL) src += c;
 
-   fclose(fpr);
-   fclose(fpw);
-cout << "now compiling..." << endl;
-   int res = system("g++ code/converted_code.cpp");
-   if (res==0){
-      cout << "Completed!" << endl;
-      system(".\\a.exe");
-   }
+    convert(src);
+    fprintf(fpw, "%s", src.c_str());
+
+    fclose(fpr);
+    fclose(fpw);
+
+    cout << "Now compiling..." << endl;
+    int res;
+    switch (pro){
+        case 'a': res = system("g++ code/converted_a.cpp"); break;
+        case 'b': res = system("g++ code/converted_b.cpp"); break;
+        case 'c': res = system("g++ code/converted_c.cpp"); break;
+        case 'd': res = system("g++ code/converted_d.cpp"); break;
+    }
+    if (res==0){
+        cout << "Compiled!" << endl;
+        cout << ">>" << endl;
+        system(".\\a.exe");
+    }
 }
