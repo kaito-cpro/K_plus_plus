88ab52be8ce7cca1597da72d51f4f7bbd2afbc24
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-07 19:18:05 +0900

    Update for.cpp

--- src/details/for.cpp
+++ src/details/for.cpp
@@ -5,16 +5,25 @@ void for_(string& src){
    while (1){
       if ((idx=find(src, "for"))<0) break;
       else {
+         int cntk = 1;
          int i = idx+4;
          string var, ran, arr;
          bool flg = true;
          while (flg){
             if (src[i]==','){
-               i += 2;
+               i += 1;
                while (1){
-                  if (src[i]==')'){
-                     flg = false;
-                     break;
+                  if (src[i]=='('){
+                      ++cntk;
+                      ran += src[i];
+                  }
+                  else if (src[i]==')'){
+                     --cntk;
+                     if (cntk==0){
+                         flg = false;
+                         break;
+                     }
+                     else ran += src[i];
                   }
                   else ran += src[i];
                   ++i;
@@ -23,9 +32,17 @@ void for_(string& src){
             else if (src[i]==' '){
                i += 4;
                while (1){
-                  if (src[i]==')'){
-                     flg = false;
-                     break;
+                  if (src[i]=='('){
+                      ++cntk;
+                      arr += src[i];
+                  }
+                  else if (src[i]==')'){
+                     --cntk;
+                     if (cntk==0){
+                         flg = false;
+                         break;
+                     }
+                     else arr += src[i];
                   }
                   else arr += src[i];
                   ++i;
@@ -34,8 +51,8 @@ void for_(string& src){
             else var += src[i];
             ++i;
          }
-         if (arr=="") src = src.substr(0, idx) + "@for (int " + var + " = 1; " + var +" <= " + ran + "; ++" + var + ")" + src.substr(i);
-         else src = src.substr(0, idx) + "@for (int " + var + " = 1; " + var +" <= " + size(arr) + "; ++" + var + ")" + src.substr(i);
+         if (arr=="") src = src.substr(0, idx) + "@for (int " + var + " = 1; " + var +" @<= " + ran + "; ++" + var + ")" + src.substr(i);
+         else src = src.substr(0, idx) + "@for (int " + var + " = 1; " + var +" @<= size(" + arr + "); ++" + var + ")" + src.substr(i);
       }
    }
 }
