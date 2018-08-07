ebe38fa328f72a1f0afd60e269b98f627e7196d6
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-07 19:19:44 +0900

    Update out.cpp

--- src/details/out.cpp
+++ src/details/out.cpp
@@ -17,9 +17,14 @@ void out_(string& src){
    while (1){
       if ((idx=find(src, "out("))<0) break;
       else {
+         int cntk = 1;
          int i = idx+4;
          while (1){
-            if (src[i]==')') break;
+            if (src[i]=='(') ++cntk;
+            else if (src[i]==')'){
+                --cntk;
+                if (cntk==0) break;
+            }
             ++i;
          }
          src = src.substr(0, idx) + "cout << " + src.substr(idx+4, i-idx-4) + " << endl;" + src.substr(i+1);
