bcb0c9e412c710fc5dc61e6a67b434aa07fd88d6
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-07 19:19:03 +0900

    Update indent.cpp

--- src/details/indent.cpp
+++ src/details/indent.cpp
@@ -3,9 +3,12 @@
 void indent_(string& src){
    src = tab + src;
    int i = 0;
-   while (src[i]){
-      if (src[i]=='\n') src.insert(i+1, tab);
-      ++i;
+   while (i<src.size()){
+      if (src[i]=='\n'){
+          src = src.substr(0, i) + ";\n" + tab + src.substr(i+1);
+          i += (";"+tab).size()+1;
+      }
+      else ++i;
    }
    src = src.substr(0, src.size()-tab.size());
 }
