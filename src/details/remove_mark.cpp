7432f11f0374c4a8d4293445d517769f6ba444a0
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-07 19:21:50 +0900

    Create remove_mark.cpp

---
+++ src/details/remove_mark.cpp
@@ -0,0 +1,12 @@
+#include "../../hdr/prototype.h"
+
+void remove_mark_(string& src){
+    int i = 0;
+    while (i<src.size()){
+        if (src[i]=='@' || src[i]=='$'){
+            src = src.substr(0, i) + src.substr(i+1);
+            --i;
+        }
+        ++i;
+    }
+}
