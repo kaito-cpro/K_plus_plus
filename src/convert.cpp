6d4e804f72f7a04a9d2b29e4611ebaa82f8ca1af
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-09 00:58:19 +0900

    Update convert.cpp

--- src/convert.cpp
+++ src/convert.cpp
@@ -5,11 +5,15 @@ void convert(string& src){
     out_(src);
     for_(src);
     kstr_(src);
-    original_format_(src);
-    indent_(src);
+    variable_(src);
     semicolon_(src);
+    indent_(src);
     if_else_(src);
+    original_format_(src);
     include_(src);
     shift_index_(src);
     remove_mark_(src);
+    //変数管理の表示
+    for(auto itr = dict.begin(); itr != dict.end(); ++itr)
+        cout << "key = " << setw(8) << left <<  itr->first << ", val = " << itr->second << endl;
 }
