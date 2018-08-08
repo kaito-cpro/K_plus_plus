e4e83eeb1d6cd0cb19237b870ff4025b339e15ed
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-09 00:56:56 +0900

    Update prototype.h

--- hdr/prototype.h
+++ hdr/prototype.h
@@ -9,6 +9,7 @@ void in_(string& src);
 void out_(string& src);
 void for_(string& src);
 void if_else_(string& src);
+void variable_(string& src);
 void kstr_(string& src);
 void original_format_(string& src);
 void shift_index_(string& src);
