81de21067e8bef69781c5ca2dbfa8e3b71db1fcf
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-11 07:10:05 +0900

    Update source_d.cpp

--- code/source_d.cpp
+++ code/source_d.cpp
@@ -1,1 +1,56 @@
 
+//様々な機能
+
+//色々対応可能;
+in(n, m, arr[n], kstr str, double d[n][m])
+
+//型は省略可能な場合がある;
+ans << ; count << 0; f << false
+
+//forは{}がないとき、その行全体をスコープとする;
+for(i in str) out(str[i]); ++count
+
+//forは色々対応可能;
+for(i in (10~3), (1~size(arr))) out(i)
+
+a << 2; b << 3; c << 4
+
+//不等号を２つ一気に使える;
+if(a <= b <= c) f = true
+
+//maxは引数は何個でもOK;
+max1 << max(a, b, c)
+
+//maxは配列にも対応;
+max2 << max(arr)
+
+//outも色々対応可能;
+out("hoge ", count, " ", str, -endl)
+
+
+kluster<int, kstr, int> klst <<
+
+t << 0
+
+for(i, min(size(arr), size(str))){
+
+    //klstへpush;
+    push(klst, (t, str, arr[i]))
+
+    ++t
+
+}
+
+//指定した要素でsort可能;
+sort(klst, 3)
+
+//klusterオブジェクトへのアクセス;
+out(klst[1][2])
+
+//substrを使いやすく改良;
+kstr u << sub(str, 3~6)
+
+//競プロ用の関数も定義;
+yes()
+
+out("END OF INTRODUCTION")
