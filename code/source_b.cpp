6d463ae75ff9e3e40a338afdd4091f0387a5c925
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-11 07:09:28 +0900

    Update source_b.cpp

--- code/source_b.cpp
+++ code/source_b.cpp
@@ -1,1 +1,20 @@
 
+//サンプルコード
+//ABC093-A
+
+in(a, b, k)
+kluster<int> klst <<
+for(i in (0~k-1)){
+    flg << true
+    if(a+i<=b){
+        for(j, size(klst)) if(klst[j]==a+i) flg = false; break
+        if(flg) out(a+i); push(klst, a+i)
+    }
+}
+for(i in (k-1~0)){
+    flg << true
+    if(b-i>=a){
+        for(j, size(klst)) if(klst[j]==b-i) flg = false; break
+        if(flg) out(b-i); push(klst, b-i)
+    }
+}
