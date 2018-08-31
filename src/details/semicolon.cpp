a0e3c031ef9d6166fd9bcda22005d62b0709bc00
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:06:34 +0900

    Update semicolon.cpp

--- src/details/semicolon.cpp
+++ src/details/semicolon.cpp
@@ -2,18 +2,28 @@
 
 void semicolon_(string& src){
     int i = 1;
-    while (1){
-        if (i==src.size()) break;
-        if (src[i]=='}' && src[i-1]!='@'){
+    while(i<src.size()){
+        if(src[i]=='\n'){
             src = src.substr(0, i) + ";" + src.substr(i);
             ++i;
         }
-        else if (src[i]=='{' && src[i-1]!='@'){
-            src = src.substr(0, i+1) + ";" + src.substr(i+1);
+        else if(src[i]=='{'&&src[i-1]!='@'){
+            int j = i-1;
+            bool flg = true;
+            while(1){
+                if(src[j]!=' '){
+                    if(src[j]=='<'&&src[j-1]=='<'){
+                        flg = false;
+                    }
+                    break;
+                }
+                else --j;
+            }
+            if(flg) src = src.substr(0, i+1) + ';' + src.substr(i+1);
             ++i;
         }
-        else if (src[i]=='\n'){
-            src = src.substr(0, i) + ";" + src.substr(i);
+        else if(src[i]=='}'&&src[i-1]!='@'){
+            src = src.substr(0, i+1) + ';' + src.substr(i+1);
             ++i;
         }
         ++i;
