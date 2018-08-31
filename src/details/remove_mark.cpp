5bd554718799a12b4b11b2bac2e2e1412477dab6
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:06:21 +0900

    Update remove_mark.cpp

--- src/details/remove_mark.cpp
+++ src/details/remove_mark.cpp
@@ -2,8 +2,8 @@
 
 void remove_mark_(string& src){
     int i = 0;
-    while (i<src.size()){
-        if (src[i]=='@' || src[i]=='$'){
+    while(i<src.size()){
+        if(src[i]=='@'||src[i]=='$'){
             src = src.substr(0, i) + src.substr(i+1);
             --i;
         }
