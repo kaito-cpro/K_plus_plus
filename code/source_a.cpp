dd93484eecec49541143ad14333eee567466e71e
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:13:28 +0900

    Update source_a.cpp

--- code/source_a.cpp
+++ code/source_a.cpp
@@ -2,9 +2,9 @@
 //サンプルコード
 //ABC093-A
 
-in(kstr s)
+in(string s)
 kluster<char> klst <<
 for(i in s) push(klst, s[i])
 sort(klst)
-if(klst[1]=='a'&&klst[2]=='b'&&klst[3]=='c') yes()
+if(klst[0]=='a'&&klst[1]=='b'&&klst[2]=='c') yes()
 else no()
