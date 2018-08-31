2ac24035ae4ce5236442c5d19e6808412f5577be
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:01:07 +0900

    Update common.cpp

--- src/common.cpp
+++ src/common.cpp
@@ -1,35 +1,47 @@
 #include "../hdr/common.h"
 
 int find(string src, string s){
-    while (1){
-        int idx, start = 0;
+    while(1){
+        int idx = INF, start = 0;
         bool flg = false;
 
-        if ((idx=(src.substr(start)).find(' '+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('\n'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('('+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find(')'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('{'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('}'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('<'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('&'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('|'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('='+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('+'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('-'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('*'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('/'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('%'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('!'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find(','+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find(';'+s))!=string::npos) flg = true;
-        else if ((idx=(src.substr(start)).find('?'+s))!=string::npos) flg = true;
-        else if (src.find(s)==0) return 0;
+        if((src.substr(start)).find(' '+s)!=string::npos&&idx>(src.substr(start)).find(' '+s))   flg = true, idx = (src.substr(start)).find(' '+s);
+        if((src.substr(start)).find('\n'+s)!=string::npos&&idx>(src.substr(start)).find('\n'+s)) flg = true, idx = (src.substr(start)).find('\n'+s);
+        if((src.substr(start)).find('('+s)!=string::npos&&idx>(src.substr(start).find('('+s)))   flg = true, idx = (src.substr(start)).find('('+s);
+        if((src.substr(start)).find(')'+s)!=string::npos&&idx>(src.substr(start).find(')'+s)))   flg = true, idx = (src.substr(start)).find(')'+s);
+        if((src.substr(start)).find('{'+s)!=string::npos&&idx>(src.substr(start).find('{'+s)))   flg = true, idx = (src.substr(start)).find('{'+s);
+        if((src.substr(start)).find('}'+s)!=string::npos&&idx>(src.substr(start).find('}'+s)))   flg = true, idx = (src.substr(start)).find('}'+s);
+        if((src.substr(start)).find('['+s)!=string::npos&&idx>(src.substr(start).find('['+s)))   flg = true, idx = (src.substr(start)).find('['+s);
+        if((src.substr(start)).find(']'+s)!=string::npos&&idx>(src.substr(start).find(']'+s)))   flg = true, idx = (src.substr(start)).find(']'+s);
+        if((src.substr(start)).find('<'+s)!=string::npos&&idx>(src.substr(start).find('<'+s)))   flg = true, idx = (src.substr(start)).find('<'+s);
+        if((src.substr(start)).find('&'+s)!=string::npos&&idx>(src.substr(start).find('&'+s)))   flg = true, idx = (src.substr(start)).find('&'+s);
+        if((src.substr(start)).find('|'+s)!=string::npos&&idx>(src.substr(start).find('|'+s)))   flg = true, idx = (src.substr(start)).find('|'+s);
+        if((src.substr(start)).find('='+s)!=string::npos&&idx>(src.substr(start).find('='+s)))   flg = true, idx = (src.substr(start)).find('='+s);
+        if((src.substr(start)).find('+'+s)!=string::npos&&idx>(src.substr(start).find('+'+s)))   flg = true, idx = (src.substr(start)).find('+'+s);
+        if((src.substr(start)).find('-'+s)!=string::npos&&idx>(src.substr(start).find('-'+s)))   flg = true, idx = (src.substr(start)).find('-'+s);
+        if((src.substr(start)).find('*'+s)!=string::npos&&idx>(src.substr(start).find('*'+s)))   flg = true, idx = (src.substr(start)).find('*'+s);
+        if((src.substr(start)).find('/'+s)!=string::npos&&idx>(src.substr(start).find('/'+s)))   flg = true, idx = (src.substr(start)).find('/'+s);
+        if((src.substr(start)).find('%'+s)!=string::npos&&idx>(src.substr(start).find('%'+s)))   flg = true, idx = (src.substr(start)).find('%'+s);
+        if((src.substr(start)).find('!'+s)!=string::npos&&idx>(src.substr(start).find('!'+s)))   flg = true, idx = (src.substr(start)).find('!'+s);
+        if((src.substr(start)).find(','+s)!=string::npos&&idx>(src.substr(start).find(','+s)))   flg = true, idx = (src.substr(start)).find(','+s);
+        if((src.substr(start)).find(';'+s)!=string::npos&&idx>(src.substr(start).find(';'+s)))   flg = true, idx = (src.substr(start)).find(';'+s);
+        if((src.substr(start)).find('?'+s)!=string::npos&&idx>(src.substr(start).find('?'+s)))   flg = true, idx = (src.substr(start)).find('?'+s);
+        if((src.substr(start)).find('$'+s)!=string::npos&&idx>(src.substr(start).find('$'+s)))   flg = true, idx = (src.substr(start)).find('$'+s);
+        if(src.find(s)==0) return 0;
 
-        if (flg){
+        if(flg){
             if (src[start+idx]!='@') return 1+start+idx;
             else ++start;
         }
         else return -1;
+    }
+}
+
+void WaitTime(int msec){
+    clock_t start, end;
+    start = clock();
+    while(1){
+        end = clock();
+        if(end-start >= msec) break;
     }
 }
