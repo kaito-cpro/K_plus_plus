ff68b0fbf75c430e0ba4b74bdf179c659c4d6b03
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:02:36 +0900

    Update array.cpp

--- src/details/array.cpp
+++ src/details/array.cpp
@@ -106,155 +106,29 @@ void sort_(string& src){
             src = src.substr(0, idx) + "@sort(" + name + ", " + name + "+" + "size(" + name + "));" + src.substr(i+1);
         }
         else if (st!=""){
-            src = src.substr(0, idx) + "@sort(" + name + "+" + st + "-1, " + name + "+" + en + ");" + src.substr(i+1);
+            src = src.substr(0, idx) + "@sort(" + name + "+" + st + ", " + name + "+" + en + "+1);" + src.substr(i+1);
         }
         else {
-            KLUSTER:
-            if (flg==false){
-                src = src.substr(0, idx) + "@sort(" + name + ".begin(), " + name + ".end());" + src.substr(i+1);
+            KLUSTER:;
+            if(flg){
+                ++i;
+                string n;
+                while(1){
+                    if(src[i]==')'){
+                        break;
+                    }
+                    else if(src[i]==' '){
+                        ++i;
+                    }
+                    else {
+                        n += src[i];
+                        ++i;
+                    }
+                }
+                src = src.substr(0, idx) + "@sort(" + name + ".begin(), " + name + ".end(), comp_" + name + "_" + n + "_);" + src.substr(i+1);
             }
             else {
-                string arg;
-                ++i;
-                while (1){
-                    if (src[i]==')'){
-                        break;
-                    }
-                    else if (src[i]==' '){
-                        ++i;
-                    }
-                    else {
-                        arg+= src[i];
-                        ++i;
-                    }
-                }
-                string ins;
-                string sub;
-                string tp;
-                int cpnnum = 1;
-                if (arg=="1"){
-                    string t = dict[name].substr(8);
-                    int pti = 0;
-                    while (1){
-                        if (t[pti]==','){
-                            ++cpnnum;
-                            ++pti;
-                        }
-                        else if (t[pti]=='>'){
-                            break;
-                        }
-                        else ++pti;
-                    }
-                    for (int kkk = 0; kkk < cpnnum-1; ++kkk){
-                        sub += ".first";
-                    }
-                    int po = 0;
-                    while (1){
-                        if (t[po]!=' ') break;
-                        ++po;
-                    }
-                    while (1){
-                        if (t[po]=='>' || t[po]==','){
-                            break;
-                        }
-                        else if (t[po]!=' '){
-                            tp += t[po];
-                            ++po;
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                }
-                else {
-                    string u = dict[name].substr(8);
-                    int po = 0;
-                    if (1){
-                        int ctcm = 0;
-                        while (1){
-                            if (u[po]==','){
-                                ++ctcm;
-                                if (ctcm==stoi(arg)-1){
-                                    break;
-                                }
-                                else ++po;
-                            }
-                            else ++po;
-                        }
-                        ++po;
-                    }
-                    while (1){
-                        if (u[po]!=' ') break;
-                        ++po;
-                    }
-                    while (1){
-                        if (u[po]=='>' || u[po]==','){
-                            break;
-                        }
-                        else if (u[po]!=' '){
-                            tp += u[po];
-                            ++po;
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                    cpnnum = 1;
-                    int pti = 0;
-                    string t = dict[name].substr(8);
-                    while (1){
-                        if (t[pti]==','){
-                            ++cpnnum;
-                            ++pti;
-                        }
-                        else if (t[pti]=='>'){
-                            break;
-                        }
-                        else ++pti;
-                    }
-                    for (int kkk = 0; kkk < cpnnum-stoi(arg); ++kkk){
-                        sub += ".first";
-                    }
-                    sub += ".second";
-                }
-                string tt = dict[name].substr(8);
-                vector<string> vt;
-                int g = 0;
-                string sg;
-                while (1){
-                    if (tt[g]==' '){
-                        ++g;
-                    }
-                    else if (tt[g]==','){
-                        vt.push_back(sg);
-                        sg = "";
-                        ++g;
-                    }
-                    else if (tt[g]=='>'){
-                        vt.push_back(sg);
-                        break;
-                    }
-                    else {
-                        sg += tt[g];
-                        ++g;
-                    }
-                }
-                ins += "@for(int i_" + to_string(num) + "_" + " = 0; i_" + to_string(num) + "_ @< size(" + name + ")-1; ++i_" + to_string(num) + "_){\n";
-                ins += tab + "@for(int j_" + to_string(num) + "_" + " = size(" + name + ")-1; j_" + to_string(num) + "_ > i_" + to_string(num) + "_; --j_" + to_string(num) + "_){\n";
-                ins += tab + tab + "@if(" + name + "@[$j_" + to_string(num) + "_-1]" + sub + ">" + name + "@[$j_" + to_string(num) + "_]" + sub + "){\n";
-                sub = "";
-                for (int ij = 0; ij < cpnnum-1; ++ij) sub += ".first";
-                for (int sl = 0; sl < cpnnum; ++sl){
-                    ins += tab + tab + tab + vt[sl] + " tmp_" + to_string(num) + "_" + to_string(sl) + "_ = " + name + "@[$j_" + to_string(num) + "_]" + sub + ";\n";
-                    ins += tab + tab + tab + name + "@[$j_" + to_string(num) + "_]" + sub + " = " + name + "@[$j_" + to_string(num) + "_-1]" + sub + ";\n";
-                    ins += tab + tab + tab + name + "@[$j_" + to_string(num) + "_-1]" + sub + " = tmp_" + to_string(num) + "_" + to_string(sl) + "_" + ";\n";
-                    if (sl==0) sub = sub.substr(0, 6*(cpnnum-2)) + ".second";
-                    else sub = sub.substr(6);
-                }
-                ins += tab + tab + ";}\n" + tab + ";}\n;}\n";
-
-                src = src.substr(0, idx) + ins + src.substr(i+1);
-                ++num;
+                src = src.substr(0, idx) + "@sort(" + name + ".begin(), " + name + ".end(), comp_" + name + "_);" + src.substr(i+1);
             }
         }
     }
@@ -338,9 +212,9 @@ void max_(string& src){
             }
             string ins;
             ins += "\n" + type + " mx_" + to_string(num) + "_ = " + vs[0] + "[$0];\n";
-            ins += "@for(int i_" + to_string(num+1) + "_ = 0; i_" + to_string(num+1) + "_ @< size(" + vs[0] + "); ++i_" + to_string(num+1) + "_){\n";
-            ins += tab + "@if(mx_" + to_string(num) + "_@<" + vs[0] + "[$i_" + to_string(num+1) + "_]) mx_" + to_string(num) + "_ = " + vs[0] + "[$i_" + to_string(num+1) + "_];\n";
-            ins += "}";
+            ins += "@for(int i_" + to_string(num+1) + "_ = 0; i_" + to_string(num+1) + "_ @< size(" + vs[0] + "); ++i_" + to_string(num+1) + "_){;\n";
+            ins += tab + "@if(mx_" + to_string(num) + "_@<" + vs[0] + "[$i_" + to_string(num+1) + "_]) {; mx_" + to_string(num) + "_ = " + vs[0] + "[$i_" + to_string(num+1) + "_];};\n";
+            ins += "};";
             src = src.substr(0, j+1) + ins + src.substr(j+1, idx-j-1) + "mx_" + to_string(num) + "_" + src.substr(i+1);
             num += 2;
         }
@@ -432,9 +306,9 @@ void min_(string& src){
             }
             string ins;
             ins += "\n" + type + " mn_" + to_string(num) + "_ = " + vs[0] + "[$0];\n";
-            ins += "@for(int i_" + to_string(num+1) + "_ = 0; i_" + to_string(num+1) + "_ @< size(" + vs[0] + "); ++i_" + to_string(num+1) + "_){\n";
-            ins += tab + "@if(mn_" + to_string(num) + "_@<" + vs[0] + "[$i_" + to_string(num+1) + "_]) mn_" + to_string(num) + "_ = " + vs[0] + "[$i_" + to_string(num+1) + "_];\n";
-            ins += "}";
+            ins += "@for(int i_" + to_string(num+1) + "_ = 0; i_" + to_string(num+1) + "_ @< size(" + vs[0] + "); ++i_" + to_string(num+1) + "_){;\n";
+            ins += tab + "@if(mn_" + to_string(num) + "_>" + vs[0] + "[$i_" + to_string(num+1) + "_]) {; mn_" + to_string(num) + "_ = " + vs[0] + "[$i_" + to_string(num+1) + "_];};\n";
+            ins += "};";
             src = src.substr(0, j+1) + ins + src.substr(j+1, idx-j-1) + "mn_" + to_string(num) + "_" + src.substr(i+1);
             num += 2;
         }
