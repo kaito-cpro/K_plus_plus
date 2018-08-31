ad253c1c0d42a206ca55f296501e94cc6c9da78b
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:01:26 +0900

    Update convert.cpp

--- src/convert.cpp
+++ src/convert.cpp
@@ -1,20 +1,42 @@
 #include "../hdr/convert.h"
 
 void convert(string& src){
-    in_(src);
-    out_(src);
-    for_(src);
-    variable_(src);
+    static bool flg = false;
     semicolon_(src);
+    cout << "semicolon" << endl;
+    bracket_(src);
+    cout << "bracket" << endl;
+
+    variable_(src);
+    cout << "variable" << endl;
+    in_(src);
+    cout << "in" << endl;
+    out_(src);
+    cout << "out" << endl;
+    for_(src);
+    cout << "for" << endl;
     array_(src);
-    kstr_(src);
+    cout << "array" << endl;
+    string_(src);
+    cout << "string" << endl;
     kluster_(src);
-    indent_(src);
-    if_else_(src);
+    cout << "kluster" << endl;
     original_format_(src);
-    include_(src);
-    shift_index_(src);
+    cout << "original_format" << endl;
+    else_process_(src);
+    cout << "else_prcess" << endl;
     remove_mark_(src);
+    cout << "remove_mark" << endl;
+
+    if(flg) return;
+    flg = true;
+    convert(pre);
+
+    indent_(src);
+    cout << "indent" << endl;
+    include_(src);
+    cout << "include" << endl;
+
     //変数管理の表示
     for(auto itr = dict.begin(); itr != dict.end(); ++itr)
         cout << "name = " << setw(8) << left <<  itr->first << ", type = " << itr->second << endl;
