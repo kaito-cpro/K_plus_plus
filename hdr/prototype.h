0bea7d1ffa3f73009cf3996e155d560b5964b755
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-08-31 17:00:19 +0900

    Update prototype.h

--- hdr/prototype.h
+++ hdr/prototype.h
@@ -2,19 +2,19 @@
 #define _INC_PROTOTYPE_
 #include "common.h"
 
-void include_(string& src);
-void indent_(string& src);
 void semicolon_(string& src);
-void in_(string& src);
-void out_(string& src);
-void for_(string& src);
-void if_else_(string& src);
+void bracket_(string& src);
 void variable_(string& src);
-void kstr_(string& src);
-void kluster_(string& src);
 void array_(string& src);
+void for_(string& src);
+void in_(string& src);
+void kluster_(string& src);
 void original_format_(string& src);
-void shift_index_(string& src);
+void out_(string& src);
+void string_(string& src);
+void else_process_(string& src);
+void indent_(string& src);
 void remove_mark_(string& src);
+void include_(string& src);
 
 #endif
