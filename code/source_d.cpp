44e633efff8a94ff1f600d3f152c37defe26b7f4
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-09-07 23:21:25 +0900

    Update source_d.cpp

--- code/source_d.cpp
+++ code/source_d.cpp
@@ -1,30 +1,30 @@
 
-//;様々な機能
+/+様々な機能+/
 
-//;色々対応可能;
+/+色々対応可能+/
 in(n, m, arr[n], string str, double d[n][m])
 
-//;型は省略可能な場合がある;
+/+型は省略可能な場合がある+/
 ans <<; count << 0; f << false
 
-//;forは{}がないとき、その行全体をスコープとする;
+/+forは{}がないとき、その行全体をスコープとする+/
 for(i in str) out(str[i]); ++count
 
-//;forは色々対応可能;
+/+forは色々対応可能+/
 for(i in (10~3), (1~size(arr))) out(i)
 
 a << 2; b << 3; c << 4
 
-//;不等号を２つ一気に使える;
+/+不等号を２つ一気に使える+/
 if(a <= b <= c) f = true
 
-//;maxは引数は何個でもOK;
+/+maxは引数は何個でもOK+/
 max1 << max(a, b, c)
 
-//;maxは配列にも対応;
+/+maxは配列にも対応+/
 max2 << max(arr)
 
-//;outも色々対応可能;
+/+outも色々対応可能+/
 out("hoge ", count, " ", str, -endl)
 
 
@@ -34,23 +34,23 @@ t << 0
 
 for(i, min(size(arr), size(str))){
 
-    //;klstへpush;
+    /+klstへpush+/
     push(klst, (t, str, arr[i]))
 
     ++t
 
 }
 
-//;指定した要素でsort可能;
+/+指定した要素でsort可能+/
 sort(klst, 0)
 
-//;klusterオブジェクトへのアクセス;
+/+klusterオブジェクトへのアクセス+/
 out(klst[0][2])
 
-//;substrを使いやすく改良;
+/+substrを使いやすく改良+/
 string u << sub(str, 3~6)
 
-//;競プロ用の関数も定義;
+/+競プロ用の関数も定義+/
 yes()
 
 out("END OF INTRODUCTION")
